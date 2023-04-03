c3bfee692ee8ee7b36cf74c7f1b28dd2309e0426
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-04-03 17:46:44 +0200

    Aufgabe2

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 var http = require('http');
+var { readFileSync } = require('fs');
 
 var server = http.createServer(function (req, res) {
-  var name = req.url.split('=');
+  var indexHTML = readFileSync('./index.html');
   res.writeHead(200);
-  res.end('Hello ' + name[1]);
+  res.end(indexHTML);
 });
 
 server.listen(3000);
 
 console.log('Server running at port 3000');
 
-// -- OTHER SOLUTION --
-// var http = require('http');
-// var url = require('url');
-// var querystring = require('querystring');
-
-// var server = http.createServer(function (req, res) {
-//   var query = url.parse(req.url).query;
-//   var parsed = querystring.parse(query);
-//   res.writeHead(200);
-//   res.end('Hello ' + parsed.username);
-// });
-
-// server.listen(3000);
-
-// console.log('Server running at port 3000');
+//  var txt = readFileSync('./tmp.txt', 'utf8');
