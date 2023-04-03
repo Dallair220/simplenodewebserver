fa619f1defa1ed280a22936827e0597402402e28
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-04-03 17:35:29 +0200

    Aufgabe1

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var http = require("http");
+var http = require('http');
 
-var server = http.createServer(function(req,res){
-    res.writeHead(200);
-    res.end("Hello World");
+var server = http.createServer(function (req, res) {
+  var name = req.url.split('=');
+  res.writeHead(200);
+  res.end('Hello ' + name[1]);
 });
 
 server.listen(3000);
 
-console.log("Server running at port 3000");
+console.log('Server running at port 3000');
+
+// -- OTHER SOLUTION --
+// var http = require('http');
+// var url = require('url');
+// var querystring = require('querystring');
+
+// var server = http.createServer(function (req, res) {
+//   var query = url.parse(req.url).query;
+//   var parsed = querystring.parse(query);
+//   res.writeHead(200);
+//   res.end('Hello ' + parsed.username);
+// });
+
+// server.listen(3000);
+
+// console.log('Server running at port 3000');
