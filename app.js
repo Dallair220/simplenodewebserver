d899eba8199b2b702a355eae19bfb437869520d0
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-04-03 17:53:38 +0200

    Aufgabe2.2

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var http = require('http');
 var { readFileSync } = require('fs');
 
 var server = http.createServer(function (req, res) {
-  var indexHTML = readFileSync('./index.html');
+  var html;
+  if (req.url === '/gummersbach') {
+    html = readFileSync('./gummersbach.html');
+  } else {
+    var html = readFileSync('./index.html');
+  }
   res.writeHead(200);
-  res.end(indexHTML);
+  res.end(html);
 });
 
 server.listen(3000);
 
 console.log('Server running at port 3000');
-
-//  var txt = readFileSync('./tmp.txt', 'utf8');
